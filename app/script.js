543cb254d2ca646549bf7c273efa2cc56ff1430a
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-08-30 15:34:41 +0530

    simplify js

--- app/script.js
+++ app/script.js
@@ -1,6 +1,7 @@
 const ageInput = document.querySelector("#input-no");
 const eligibility = document.querySelector("#btn-eligibility");
 const clear = document.querySelector("#btn-clear");
+const result = document.querySelector("#result-show");
 clear.addEventListener("click", function () {
   document.querySelector("#input-no").value = "";
   document.getElementById("result-show").style.display = "none";
@@ -8,18 +9,16 @@ clear.addEventListener("click", function () {
 eligibility.addEventListener("click", function () {
   const userAge = ageInput.value;
   if (!userAge) {
-    document.querySelector("#result-show").innerHTML = "Please Enter Age";
+    result.innerHTML = "Please Enter Age";
     return;
   }
   if (userAge < 18) {
-    document.querySelector("#result-show").innerHTML =
-      "You are not eligible to vote";
-    document.querySelector("#result-show").style.color = "red";
-    document.getElementById("result-show").style.display = "block";
+    result.innerHTML = "You are not eligible to vote";
+    result.style.color = "red";
+    result.style.display = "block";
   } else {
-    document.querySelector("#result-show").innerHTML =
-      "You are  eligible to vote";
-    document.querySelector("#result-show").style.color = "green";
-    document.getElementById("result-show").style.display = "block";
+    result.innerHTML = "You are  eligible to vote";
+    result.style.color = "green";
+    result.style.display = "block";
   }
 });
